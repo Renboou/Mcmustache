ff751831514e05f3ddf513dc38a30559cb8017e4
Renboou <94653387+Renboou@users.noreply.github.com>
2023-01-23 18:53:56 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+function preload() {
+mcmustache = loadImage("https://i.postimg.cc/g2MhMvFZ/mcdonald.png");
+}
+
+function setup() {
+canvas = createCanvas(300,300);
+canvas.center();
+}
+
+function draw(){}
+
+function take_snapshot(){
+save('coolMcmustache.png')
+}
+
+
+
