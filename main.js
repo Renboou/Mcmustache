75c8478963af0e974ea23358681149602f6479db
Renboou <94653387+Renboou@users.noreply.github.com>
2023-01-30 18:42:36 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,55 @@
-
-
 function preload() {
-mcmustache = loadImage("https://i.postimg.cc/g2MhMvFZ/mcdonald.png");
-}
-
-function setup() {
-canvas = createCanvas(300,300);
-canvas.center();
-}
-
-function draw(){}
-
-function take_snapshot(){
-save('coolMcmustache.png')
-}
-
-
-
+    mcmustache = loadImage("https://i.postimg.cc/g2MhMvFZ/mcdonald.png");
+    
+    
+    }
+    nosex=0;
+    nosey=0;
+    
+    
+    function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300)
+    video.hide();
+    
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+    }
+    
+    
+    function draw(){
+    image(video,0,0,300,300);
+    //circle(nosex,nosey,20);
+    image(mcmustache,nosex -100,nosey ,200,10);
+    
+    
+    }
+    
+    
+    function take_snapshot(){
+    save('myFilterImage.png');
+    }
+    
+    
+    function modelLoaded(){
+       console.log('PoseNet is on :)');
+    }
+    
+    
+    
+    
+    function gotPoses(results)
+    {
+    if(results.length > 0)
+    {
+    console.log(results);
+    nosex = results[0].pose.nose.x;
+    nosey = results[0].pose.nose.y;
+    console.log("nose x = " + nosex);
+    console.log("nose y = " + nosey);
+    }
+    }
+    
